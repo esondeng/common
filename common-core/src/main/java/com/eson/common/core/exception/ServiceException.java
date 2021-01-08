f2e82b609131124114ec454ba08abaf1187b218e
dengxiaolin <dengxiaolin@meituan.com>
2021-01-08 10:14:10 +0800

    common包建立

--- common-core/src/main/java/com/eson/common/core/exception/ServiceException.java
+++ common-core/src/main/java/com/eson/common/core/exception/ServiceException.java
@@ -1,5 +1,9 @@
 package com.eson.common.core.exception;
 
+import java.util.List;
+
+import org.apache.commons.collections4.CollectionUtils;
+
 /**
  * 微服务异常
  *
@@ -13,9 +17,9 @@ public class ServiceException extends BusinessException {
     private String interfacePath;
 
     /**
-     * first place in error stack
+     * 为了减少数据量，这里只记录错误栈前三条
      */
-    private String errorPlace;
+    private List<String> errorStack;
 
     public ServiceException() {
 
@@ -35,12 +39,21 @@ public class ServiceException extends BusinessException {
         return this;
     }
 
-    public String getErrorPlace() {
-        return errorPlace;
+    public List<String> getErrorStack() {
+        return errorStack;
     }
 
-    public ServiceException setErrorPlace(String errorPlace) {
-        this.errorPlace = errorPlace;
+    public ServiceException setErrorStack(List<String> errorStack) {
+        this.errorStack = errorStack;
         return this;
+    }
+
+    public String getErrorPlace() {
+        if (CollectionUtils.isEmpty(errorStack)) {
+            return null;
+        }
+        else {
+            return errorStack.get(0);
+        }
     }
 }
