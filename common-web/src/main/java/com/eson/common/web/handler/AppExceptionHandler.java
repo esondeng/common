2281cb6af2f2ab4677ea18817d8ed82fc1342fd8
dengxiaolin <dengxiaolin@meituan.com>
2021-01-09 19:52:23 +0800

    common包建立

--- common-web/src/main/java/com/eson/common/web/handler/AppExceptionHandler.java
+++ common-web/src/main/java/com/eson/common/web/handler/AppExceptionHandler.java
@@ -66,6 +66,7 @@ public class AppExceptionHandler {
             return logAndBuildResponse(bex);
         }
         else {
+            log.error("系统服务异常", e);
             return WebResponse.fail("系统服务异常，请稍后重试");
         }
     }
