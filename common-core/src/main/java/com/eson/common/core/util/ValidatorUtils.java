a39f86042a48dd71cb18efd48aff5368ca5ac693
dengxiaolin <dengxiaolin@meituan.com>
2021-01-22 13:15:39 +0800

    上传文件工具类

--- common-core/src/main/java/com/eson/common/core/util/ValidatorUtils.java
+++ common-core/src/main/java/com/eson/common/core/util/ValidatorUtils.java
@@ -1,6 +1,9 @@
 package com.eson.common.core.util;
 
+import java.util.Date;
+import java.util.Objects;
 import java.util.Set;
+import java.util.regex.Pattern;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
@@ -23,6 +26,17 @@ public class ValidatorUtils {
 
     private static Validator validator = factory.getValidator();
 
+
+    /**
+     * 手机号正则
+     */
+    private static Pattern phoneNoPattern = Pattern.compile("^1\\d{10}$");
+
+    /**
+     * 邮箱正则
+     */
+    private static Pattern emailPattern = Pattern.compile("^([a-z0-9A-Z]+[-|\\.|_]?)*[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$");
+
     public static <T> void validate(T target, Class<?>... groups) {
         String validateResult = getValidateResult(target, groups);
         Assert.throwIfTrue(StringUtils.isNotBlank(validateResult), validateResult);
@@ -39,5 +53,17 @@ public class ValidatorUtils {
         violationSet.forEach(violation -> message.append(violation.getMessage()).append(" ,"));
 
         return message.substring(0, message.length() - 1);
+    }
+
+    public static boolean isPhone(String phone) {
+        return StringUtils.isNotBlank(phone) && phoneNoPattern.matcher(phone).matches();
+    }
+
+    public static boolean isEmail(String email) {
+        return StringUtils.isNotBlank(email) && emailPattern.matcher(email).matches();
+    }
+
+    public static boolean isFuture(Date date) {
+        return Objects.nonNull(date) && date.toInstant().isAfter(TimeUtils.now().toInstant());
     }
 }
