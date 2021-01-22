fd2509f495a99c226cd2e295023121a9119cf108
dengxiaolin <dengxiaolin@meituan.com>
2021-01-22 13:18:54 +0800

    uuid

---
+++ common-core/src/main/java/com/eson/common/core/util/UuidUtils.java
@@ -0,0 +1,16 @@
+package com.eson.common.core.util;
+
+import java.util.UUID;
+
+/**
+ * @author dengxiaolin
+ * @since 2021/01/22
+ */
+public class UuidUtils {
+    public static String genUuid() {
+        return UUID.randomUUID()
+                .toString()
+                .replace("-", "")
+                .toLowerCase();
+    }
+}
